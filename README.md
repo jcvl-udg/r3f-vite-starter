53c23af72fc51d288b0f602e99e455bd82801730
VARGAS LOPEZ <jcarlos.vargas@udg.edu.mx>
2025-03-14 12:55:05 -0600

    char Control
    el modelo usa su animacion y rota suave, se puede desplazar por todo el mapa

--- README.md
+++ README.md
@@ -6,5 +6,12 @@ npm install
 npm run dev
 ```
 
+#### Resources 
+
+https://www.youtube.com/watch?v=yjpGVIe_Gy8
+
+17:00 - implementing animations
+mouse controller
+touch controller
 
 ![image](https://user-images.githubusercontent.com/6551176/221732091-23ee52cb-4150-42fa-b998-43628d7a6b0d.png)
--- src/components/CharacterController.jsx
+++ src/components/CharacterController.jsx
@@ -1,12 +1,32 @@
 import { CapsuleCollider, RigidBody } from "@react-three/rapier";
 import { Character } from "./Character";
-import { useRef } from "react";
-import { Vector3 } from "three";
+import { useRef, useState } from "react";
+import { MathUtils, Vector3 } from "three";
 import { useFrame } from "@react-three/fiber";
 import { cameraPosition } from "three/tsl";
 import { useControls } from "leva";
 import { useKeyboardControls } from "@react-three/drei";
+import { degToRad } from "three/src/math/MathUtils.js";
 
+const normalizeAngle = (angle) => {
+    while (angle > Math.PI) angle -= 2 * Math.PI;
+    while (angle < -Math.PI) angle += 2 * Math.PI;
+    return angle;
+};
+
+const lerpAngle = (start, end, t) => {
+    start = normalizeAngle(start);
+    end = normalizeAngle(end);
+  
+    if (Math.abs(end - start) > Math.PI) {
+      if (end > start) {
+        start += 2 * Math.PI;
+      } else {
+        end += 2 * Math.PI;
+      }
+    }
+    return normalizeAngle(start + (end - start) * t);
+};
 
 export const CharacterController = () => { 
     const container = useRef();
@@ -17,11 +37,21 @@ export const CharacterController = () => {
     const cameraWorldPosition = useRef(new Vector3());
     const cameraLookAtWorldPosition = useRef(new Vector3());
     const cameraLookAt = useRef(new Vector3());
+    // animations
+    const [animation, setAnimation] = useState("idle"); 
     // Rigid body refs 
     const rb = useRef();
-    const { WALK_SPEED, RUN_SPEED } = useControls("Character Control" , {
+    const rotationTarget = useRef(0);
+    const characterRotationTarget = useRef(0);
+    const { WALK_SPEED, RUN_SPEED , ROTATION_SPEED} = useControls("Character Control" , {
         WALK_SPEED: { value: 0.8, min: 0.1, max: 4, step: 0.1 },
-        RUN_SPEED: { value: 1.6, min: 0.2, max: 12, step: 0.1 },
+        RUN_SPEED: { value: 1.6, min: 0.2, max: 12, step: 0.1 },      
+        ROTATION_SPEED: {
+            value: degToRad(0.5),
+            min: degToRad(0.1),
+            max: degToRad(5),
+            step: degToRad(0.1),
+          },
     });
     const [, get] = useKeyboardControls();
     // Useframe 4 update cameras 
@@ -49,12 +79,41 @@ export const CharacterController = () => {
                 movement.x = -1;
               }
         
+              if (movement.x !== 0){
+                rotationTarget.current += ROTATION_SPEED * movement.x;
+              }
+
               if (movement.x !== 0 || movement.z !== 0) {
-                vel.z = speed * movement.z;
-              } 
+                characterRotationTarget.current = Math.atan2(movement.x, movement.z);
+
+                vel.x = Math.sin(rotationTarget.current + characterRotationTarget.current ) * speed;
+                vel.z = Math.cos(rotationTarget.current + characterRotationTarget.current ) * speed;
+                // vel.z = speed * movement.z;
+                // if(speed == RUN_SPEED){
+                //     // include some running animation
+                //     setAnimation("Scene");
+                // }else{
+                //     setAnimation("Scene");
+                // }
+            }
+            //   } else {
+            //     setAnimation("idle");
+            //   }
+            character.current.rotation.y = lerpAngle(
+                character.current.rotation.y,
+                characterRotationTarget.current,
+                0.1
+            );
 
             rb.current.setLinvel(vel, true);
         }
+
+        // Camaras
+        container.current.rotation.y = MathUtils.lerp(
+            container.current.rotation.y,
+            rotationTarget.current,
+            0.1
+        );
 
         cameraPosition.current.getWorldPosition(cameraWorldPosition.current);
         camera.position.lerp(cameraWorldPosition.current, 0.1);
@@ -78,7 +137,7 @@ export const CharacterController = () => {
             <group ref={cameraTarget} position-z={1.5} />    
             <group ref={cameraPosition} position-y={4} position-z={-4} /> 
                 <group ref={character} >
-                    <Character scale={0.034} animation={"idle"} />
+                    <Character scale={0.034} animation={"Scene"} />
                 </group>
         </group>
         <CapsuleCollider
